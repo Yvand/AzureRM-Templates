e5627fcc9bf6c6549ba10fa3b39fa5766d11fecd
Yvan Duhamel <yvandev@outlook.fr>
2016-10-10 14:03:58 +0200

    Working on SP16-3VM-ADFS template

---
+++ SP16-PLA/Scripts/Create-KeyVault.ps1
@@ -0,0 +1,10 @@
+﻿#Requires -Module AzureRM.Profile
+#Requires -Module AzureRM.KeyVault
+
+Param(
+    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
+    [string] $ResourceGroupName = 'SPPLA_Arm_Test',
+    [string] $VaultName = "plavault"
+)
+
+New-AzureRmKeyVault -VaultName $VaultName -ResourceGroupName $ResourceGroupName -Location $ResourceGroupLocation -EnabledForTemplateDeployment
