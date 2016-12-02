eddbbf2de1413484a9dd8cd8a50b60c3905fbccb
Yvan Duhamel <yvandev@outlook.fr>
2016-12-02 10:17:57 +0100

    Working on SampleWebSite

--- PaaS/SampleWebSite/Deploy-AzureResourceGroup.ps1
+++ PaaS/SampleWebSite/Deploy-AzureResourceGroup.ps1
@@ -1,3 +1,17 @@
+### Ensure connection to Azure RM
+$azurecontext = $null
+$azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+
+if ($azurecontext -eq $null) {
+    Login-AzureRmAccount
+    $azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+}
+
+if ($azurecontext -eq $null){ 
+    return
+}
+
+
 ### Define variables
 {
 $resourceGroupLocation = 'westeurope'
@@ -5,11 +19,8 @@ $resourceGroupName = 'samplesite-paas'
 $resourceDeploymentName = 'samplesite-paas-deployment'
 $templateFileName = 'azuredeploy.json'
 $templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $templateFileName))
+$templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine("C:\Job\Dev\Github\AzureRM-Templates\PaaS\SampleWebSite", $templateFileName))
 }
-
-<#
-Login-AzureRmAccount
-#>
 
 ### Create Resource Group
 {
