c0ad58f8a68fba55c954ea7734679a38dd9ef09b
Yvan Duhamel <yvandev@outlook.fr>
2016-12-07 17:47:16 +0100

    Working on template SP16-ADFS

---
+++ SharePoint/SP16-ADFS/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,46 @@
+### Ensure connection to Azure RM
+$azurecontext = $null
+$azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+
+if ($azurecontext -eq $null) {
+    Login-AzureRmAccount
+    $azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+}
+
+if ($azurecontext -eq $null){ 
+    return
+}
+
+
+### Define variables
+{
+$resourceGroupLocation = 'westeurope'
+$resourceGroupName = 'yd-sp16adfs'
+$resourceDeploymentName = 'yd-sp16adfs-deployment'
+$templateFileName = 'azuredeploy.json'
+$templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $templateFileName))
+$templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine("C:\Job\Dev\Github\AzureRM-Templates\SharePoint\SP16-ADFS", $templateFileName))
+$password = "****"
+$securePassword = $password| ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+$additionalParameters = New-Object -TypeName HashTable
+$additionalParameters['adminPassword'] = $securePassword
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $templatePath `
+    @additionalParameters `
+    -Verbose -Force
+}
