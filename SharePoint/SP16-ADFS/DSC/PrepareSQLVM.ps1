36d78a15a16495d2ff75269f5f13d7ba6324e53a
Yvan Duhamel <yvandev@outlook.fr>
2016-12-12 10:35:21 +0100

    Updated DSC for SQL VM

---
+++ SharePoint/SP16-ADFS/DSC/PrepareSQLVM.ps1
@@ -0,0 +1,98 @@
+#
+# Copyright="� Microsoft Corporation. All rights reserved."
+#
+
+configuration PrepareSqlServer
+{
+
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DNSServer,
+        [Int]$RetryCount=30,
+        [Int]$RetryIntervalSec=60
+    )
+
+    Import-DscResource -ModuleName xComputerManagement, xNetworking, xDisk,cDisk
+    WaitForSqlSetup
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+        }
+        cDiskNoRestart SQLDataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+	        DependsOn="[xWaitForDisk]Disk2"
+        }
+        xWaitforDisk Disk3
+        {
+            DiskNumber = 3
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
+            DependsOn="[cDiskNoRestart]SQLDataDisk"
+        }
+        cDiskNoRestart SQLLogDisk
+        {
+            DiskNumber = 3
+            DriveLetter = "G"
+            DependsOn="[xWaitForDisk]Disk3"
+        }
+        xFirewall DatabaseEngineFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+            Group = "SQL Server"
+            Enabled = "True"
+            Protocol = "TCP"
+            LocalPort = "1433"
+            Ensure = "Present"
+        }
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+            DependsOn = "[cDiskNoRestart]SQLDataDisk","[cDiskNoRestart]SQLLogDisk"
+
+        }
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DNSServer
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn="[WindowsFeature]ADPS"
+        }
+
+    }
+}
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
+
--- SharePoint/SP16-ADFS/Deploy-AzureResourceGroup.ps1
+++ SharePoint/SP16-ADFS/Deploy-AzureResourceGroup.ps1
@@ -1,16 +1,6 @@
-### Ensure connection to Azure RM
-$azurecontext = $null
-$azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
-
-if ($azurecontext -eq $null) {
-    Login-AzureRmAccount
-    $azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
-}
-
-if ($azurecontext -eq $null){ 
-    return
-}
-
+#Requires -Version 3.0
+#Requires -Module AzureRM.Resources
+#Requires -Module Azure.Storage
 
 ### Define variables
 {
@@ -18,29 +8,57 @@ $resourceGroupLocation = 'westeurope'
 $resourceGroupName = 'yd-sp16adfs'
 $resourceDeploymentName = 'yd-sp16adfs-deployment'
 $templateFileName = 'azuredeploy.json'
-$templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $templateFileName))
-$templatePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine("C:\Job\Dev\Github\AzureRM-Templates\SharePoint\SP16-ADFS", $templateFileName))
-$password = "****"
-$securePassword = $password| ConvertTo-SecureString -AsPlainText -Force
+$TemplateParametersFile = 'azuredeploy.parameters.json'
+$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $templateFileName))
+#$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine("C:\Job\Dev\Github\AzureRM-Templates\SharePoint\SP16-ADFS", $templateFileName))
+$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
+#$password = "****"
+#$securePassword = $password| ConvertTo-SecureString -AsPlainText -Force
+$securePassword = Read-Host "Enter the password" -AsSecureString
+
+$DSCSourceFolder = 'DSC'
 }
 
-### Create Resource Group
-{
-New-AzureRmResourceGroup `
-    -Name $resourceGroupName `
-    -Location $resourceGroupLocation `
-    -Verbose -Force
+### Ensure connection to Azure RM
+Import-Module Azure -ErrorAction SilentlyContinue
+$azurecontext = $null
+$azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+if ($azurecontext -eq $null) {
+    Login-AzureRmAccount
+    $azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+}
+if ($azurecontext -eq $null){ 
+    return
+}
+
+if ($UploadArtifacts) {
+    $ArtifactStagingDirectory = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ArtifactStagingDirectory))
+    $DSCSourceFolder = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $DSCSourceFolder))
+
+    # Create DSC configuration archive
+    if (Test-Path $DSCSourceFolder) {
+        $DSCSourceFilePaths = @(Get-ChildItem $DSCSourceFolder -File -Filter "*.ps1" | ForEach-Object -Process {$_.FullName})
+        foreach ($DSCSourceFilePath in $DSCSourceFilePaths) {
+            $DSCArchiveFilePath = $DSCSourceFilePath.Substring(0, $DSCSourceFilePath.Length - 4) + ".zip"
+            Publish-AzureRmVMDscConfiguration $DSCSourceFilePath -OutputArchivePath $DSCArchiveFilePath -Force -Verbose
+        }
+    }
+}
+
+### Create Resource Group if it doesn't exist
+if ((Get-AzureRmResourceGroup -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue) -eq $null) {
+    New-AzureRmResourceGroup `
+        -Name $resourceGroupName `
+        -Location $resourceGroupLocation `
+        -Verbose -Force
 }
 
 ### Deploy Resources
-{
 $additionalParameters = New-Object -TypeName HashTable
 $additionalParameters['adminPassword'] = $securePassword
-
 New-AzureRmResourceGroupDeployment `
     -Name $resourceDeploymentName `
     -ResourceGroupName $resourceGroupName `
-    -TemplateFile $templatePath `
+    -TemplateFile $TemplateFile `
     @additionalParameters `
     -Verbose -Force
-}
