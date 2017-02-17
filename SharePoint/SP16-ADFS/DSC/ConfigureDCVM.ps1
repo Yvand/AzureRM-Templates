f80d549170ed6fada907b412eb72b6b47bea5be5
Yvan Duhamel <yvandev@outlook.fr>
2017-02-17 09:39:08 +0100

    Testing ADFS farm creation

--- SharePoint/SP16-ADFS/DSC/ConfigureDCVM.ps1
+++ SharePoint/SP16-ADFS/DSC/ConfigureDCVM.ps1
@@ -266,7 +266,7 @@
 
                 $stdOutLog = "C:\stdout.log"
                 $stdErrLog = "C:\stderr.log"
-                Start-Process -LoadUserProfile -Credential $DomainCredsNetbios -Wait -FilePath $PSHOME\powershell.exe -ArgumentList "-Command & {$ScriptBlock CreateADFSFarm}", "$using:DomainName", $using:AdfsSvcCreds.UserName -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
+                #Start-Process -LoadUserProfile -Credential $DomainCredsNetbios -Wait -FilePath $PSHOME\powershell.exe -ArgumentList "-Command & {$ScriptBlock CreateADFSFarm}", "$using:DomainName", $using:AdfsSvcCreds.UserName -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
                 Write-Verbose -Message "ADFS farm successfully created"
             }
             GetScript =  
