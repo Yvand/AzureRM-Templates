f5e8ad7c861603cbd724080f1ac66393a0bad0fb
Yvan Duhamel <yvandev@outlook.fr>
2018-04-03 13:35:54 +0200

    Force protocol TLS 1.2 to fix GitHub download error in Invoke-WebRequest

--- SharePoint/SharePoint-ADFS/dsc/ConfigureSPVM.ps1
+++ SharePoint/SharePoint-ADFS/dsc/ConfigureSPVM.ps1
@@ -948,6 +948,8 @@ function Get-LatestGitHubRelease
         [string] $Repo,
         [string] $Artifact
     )
+    # Force protocol TLS 1.2 to avoid TLS/SSL error when downloading from GitHub, as documented in https://docs.microsoft.com/en-us/azure/azure-stack/azure-stack-update-1802
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     # Found in https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/
     $latestRelease = Invoke-WebRequest https://github.com/$Repo/releases/latest -Headers @{"Accept"="application/json"} -UseBasicParsing
     $json = $latestRelease.Content | ConvertFrom-Json
