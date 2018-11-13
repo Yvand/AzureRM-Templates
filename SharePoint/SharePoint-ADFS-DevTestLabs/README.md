84b7304bce52ff855e6399aa40063fe229a71d84
Yvan Duhamel <yvandev@outlook.fr>
2018-11-13 17:36:57 +0100

    Create template suited for DevLabs

---
+++ SharePoint/SharePoint-ADFS-DevTestLabs/README.md
@@ -0,0 +1,12 @@
+# SharePoint template for DevTest labs
+
+## Description
+
+This template deploys SharePoint 2013 or 2016 with minimum configuration to run test that involve federated authentication.
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FYvand%2FAzureRM-Templates%2Fmaster%2FSharePoint%2FSharePoint-ADFS-DevTestLabs%2Fazuredeploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FYvand%2FAzureRM-Templates%2Fmaster%2FSharePoint%2FSharePoint-ADFS-DevTestLabs%2Fazuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
---
+++ SharePoint/SharePoint-ADFS-DevTestLabs/Scripts/Generate-DSCArchive.ps1
@@ -0,0 +1,34 @@
+#Requires -Version 3.0
+#Requires -Module AzureRM.Compute
+
+param(
+    [string]$vmName = "*"
+)
+
+<#
+-vmName "FE"
+#>
+
+$azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+if ($azurecontext -eq $null -or $azurecontext.Account -eq $null -or $azurecontext.Subscription -eq $null) {
+    Write-Host "Launching Azure authentication prompt..." -ForegroundColor Green
+    Login-AzureRmAccount
+    $azurecontext = Get-AzureRmContext -ErrorAction SilentlyContinue
+}
+if ($azurecontext -eq $null -or $azurecontext.Account -eq $null -or $azurecontext.Subscription -eq $null){ 
+    Write-Host "Unable to get a valid context." -ForegroundColor Red
+    return
+}
+function Generate-DSCArchive($vmName) {
+    $dscSourceFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\dsc" -Resolve
+
+    if (Test-Path $dscSourceFolder) {
+        $dscSourceFilePaths = @(Get-ChildItem $dscSourceFolder -File -Filter "*$vmName*.ps1" | ForEach-Object -Process {$_.FullName})
+        foreach ($dscSourceFilePath in $dscSourceFilePaths) {
+            $dscArchiveFilePath = $dscSourceFilePath.Substring(0, $dscSourceFilePath.Length - 4) + ".zip"
+            Publish-AzureRmVMDscConfiguration $dscSourceFilePath -OutputArchivePath $dscArchiveFilePath -Force -Verbose
+        }
+    }
+}
+
+Generate-DSCArchive $vmName
