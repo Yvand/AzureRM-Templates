f2db7572abcbeb43c88828592713f5333f7dc4c1
Yvan Duhamel <yvandev@outlook.fr>
2019-09-30 16:45:14 +0200

    Update SharePoint-ADFS.sh

--- Templates/SharePoint-ADFS/SharePoint-ADFS.sh
+++ Templates/SharePoint-ADFS/SharePoint-ADFS.sh
@@ -2,6 +2,9 @@ location="west europe"
 resourceGroupName="ydcli"
 adminUserName="yvand"
 adminPassword=ChangeYourAdminPassword1
+artifactsURI="https://github.com/Yvand/AzureRM-Templates/raw/master/Templates/SharePoint-ADFS/"
+domainFQDN="contoso.local"
+adfsSvcUserName="svc_adfs"
 
 az group create --name $resourceGroupName --location "$location"
 
@@ -15,7 +18,7 @@ az network nsg rule create -g $resourceGroupName --nsg-name "NSG-VNet-SQL" --nam
     --source-port-ranges "*" --destination-address-prefixes '*' --destination-port-ranges 3389 --access Allow --protocol Tcp --description "Allow RDP"
 
 az network nsg create --name "NSG-VNet-SP" -g $resourceGroupName
-az network nsg rule create -g $resourceGroupName --nsg-name NSG-VNet-SP --name "allow-rdp-rule" --priority 100 --source-address-prefixes Internet \
+az network nsg rule create -g $resourceGroupName --nsg-name "NSG-VNet-SP" --name "allow-rdp-rule" --priority 100 --source-address-prefixes Internet \
     --source-port-ranges "*" --destination-address-prefixes '*' --destination-port-ranges 3389 --access Allow --protocol Tcp --description "Allow RDP"
 
 # Create virtual network
@@ -24,13 +27,12 @@ az network vnet subnet create -g $resourceGroupName --vnet-name Vnet --name "Sub
 az network vnet subnet create -g $resourceGroupName --vnet-name Vnet --name "Subnet-SQL" --address-prefixes 10.0.2.0/24 --network-security-group "NSG-VNet-SQL"
 az network vnet subnet create -g $resourceGroupName --vnet-name Vnet --name "Subnet-SP" --address-prefixes 10.0.3.0/24 --network-security-group "NSG-VNet-SQL"
 
-# # Create a VM
-az network public-ip create --allocation-method Dynamic --name pip_dc -g $resourceGroupName
-az network nic create --name nic_dc --public-ip-address pip_dc --vnet-name Vnet --subnet Subnet-DC -g $resourceGroupName
+# Create VM DC
+az network public-ip create --allocation-method Dynamic --name PublicIP-DC --dns-name "${resourceGroupName}-dc" -g $resourceGroupName
+az network nic create --name VM-DC-NIC --public-ip-address PublicIP-DC --vnet-name Vnet --subnet Subnet-DC -g $resourceGroupName
 az vm create --resource-group $resourceGroupName --name DC --image win2016datacenter --admin-username $adminUserName --admin-password $adminPassword --license-type Windows_Server \
-    --nics nic_dc --os-disk-size-gb 128 --os-disk-caching ReadWrite --os-disk-name "vm_dc_osdisk" --size Standard_F4
+    --nics VM-DC-NIC --os-disk-size-gb 128 --os-disk-caching ReadWrite --os-disk-name "VM-DC-OSDisk" --size Standard_F4
 
-az vm extension set --name DSC --publisher Microsoft.Powershell --version 2.19 --vm-name DC -g $resourceGroupName \
-   --settings '{"ModulesURL":"https://github.com/Azure/azure-quickstart-templates/raw/master/dsc-extension-iis-server-windows-vm/ContosoWebsite.ps1.zip", "configurationFunction": "ContosoWebsite.ps1\\ContosoWebsite", "Properties": {"MachineName": "myVM"} }'
-
-# az find subnet
+az vm extension set --name DSC --publisher Microsoft.Powershell --version 2.9 --vm-name DC -g $resourceGroupName \
+   --settings '{"ModulesURL": "'${artifactsURI}'dsc/ConfigureDCVM.zip", "configurationFunction": "ConfigureDCVM.ps1\\ConfigureDCVM", "Properties": {"domainFQDN": "'${domainFQDN}'", "PrivateIP": "10.0.1.4"} }' \
+   --protected-settings '{"Properties": {"AdminCreds": {"UserName": "'${adminUserName}'", "Password": "'${adminPassword}'" }, "AdfsSvcCreds": {"UserName": "'${adfsSvcUserName}'", "Password": "'${adminPassword}'" }}}'
