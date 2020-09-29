5071606dd60759ee26aed887a28cbf808c623b7c
Yvan Duhamel <yvandev@outlook.fr>
2020-09-29 09:37:01 +0200

    Update Deploy-AzureResourceGroup.ps1

--- Templates/SharePoint-ADFS/Deploy-AzureResourceGroup.ps1
+++ Templates/SharePoint-ADFS/Deploy-AzureResourceGroup.ps1
@@ -3,7 +3,7 @@
 ### Define variables
 $resourceGroupLocation = 'westeurope'
 #$resourceGroupLocation = 'northeurope'
-$resourceGroupName = 'ydqs7'
+$resourceGroupName = 'ydqs4'
 $resourceDeploymentName = "$resourceGroupName-deployment"
 $templateFileName = 'azuredeploy.json'
 $templateParametersFileName = 'azuredeploy.parameters.json'
