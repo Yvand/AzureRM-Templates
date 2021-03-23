1438c7e11ad54f24d5f02112c131aab46a79a3d9
Yvan Duhamel <yvandev@outlook.fr>
2021-03-23 13:17:19 +0100

    test

--- Templates/SharePoint-ADFS/dsc/ConfigureDCVM.ps1
+++ Templates/SharePoint-ADFS/dsc/ConfigureDCVM.ps1
@@ -8,7 +8,7 @@
         [Parameter(Mandatory)] [String]$PrivateIP
     )
 
-    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, ActiveDirectoryCSDsc, CertificateDsc, xDnsServer, ComputerManagementDsc, AdfsDsc, cADFS
+    Import-DscResource -ModuleName ActiveDirectoryDsc, NetworkingDsc, xPSDesiredStateConfiguration, ActiveDirectoryCSDsc, CertificateDsc, xDnsServer, ComputerManagementDsc, AdfsDsc
     [String] $DomainNetbiosName = (Get-NetBIOSName -DomainFQDN $DomainFQDN)
     [System.Management.Automation.PSCredential] $DomainCredsNetbios = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
     [System.Management.Automation.PSCredential] $AdfsSvcCredsQualified = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($AdfsSvcCreds.UserName)", $AdfsSvcCreds.Password)
@@ -231,38 +231,36 @@
             DependsOn = "[WaitForADDomain]WaitForDCReady"
         }
 
-        # Since 2019-10, DSC regularly fails at cADFSFarm CreateADFSFarm with error below, but I don't know why or how to fix it.
-        # Machine restart message is present even when there is no error and PendingReboot before cADFSFarm detects no pending reboot
-        # VERBOSE: [2019-10-04 11:14:42Z] [VERBOSE] [DC]: [[cADFSFarm]CreateADFSFarm] Entering function InstallADFSFarm
-        # VERBOSE: [2019-10-04 11:14:42Z] [WARNING] [DC]: [[cADFSFarm]CreateADFSFarm] A machine restart is required to complete ADFS service configuration. For more information, see: http://go.microsoft.com/fwlink/?LinkId=798725
-        # VERBOSE: [2019-10-04 11:19:14Z] [ERROR] ADMIN0121: An attempt to update service settings failed because the data set that was used for updating was stale. Refresh the data in your session or console view, and then try the update again.
-        cADFSFarm CreateADFSFarm
-        {
-            ServiceCredential         = $AdfsSvcCredsQualified
-            InstallCredential         = $DomainCredsNetbios
-            DisplayName               = "$ADFSSiteName.$DomainFQDN"
-            ServiceName               = "$ADFSSiteName.$DomainFQDN"
-            CertificateName           = "$ADFSSiteName.$DomainFQDN"
-            SigningCertificateName    = "$ADFSSiteName.Signing"
-            DecryptionCertificateName = "$ADFSSiteName.Decryption"
-            Ensure                    = 'Present'
-            PsDscRunAsCredential      = $DomainCredsNetbios
-            DependsOn                 = "[WindowsFeature]AddADFS"
-        }
-
-        # This triggers a reboot of the VM, which ruins the deployment of other VMs
-        # AdfsFarm CreateADFSFarm
+        # # Since 2019-10, DSC regularly fails at cADFSFarm CreateADFSFarm with error below, but I don't know why or how to fix it.
+        # # Machine restart message is present even when there is no error and PendingReboot before cADFSFarm detects no pending reboot
+        # # VERBOSE: [2019-10-04 11:14:42Z] [VERBOSE] [DC]: [[cADFSFarm]CreateADFSFarm] Entering function InstallADFSFarm
+        # # VERBOSE: [2019-10-04 11:14:42Z] [WARNING] [DC]: [[cADFSFarm]CreateADFSFarm] A machine restart is required to complete ADFS service configuration. For more information, see: http://go.microsoft.com/fwlink/?LinkId=798725
+        # # VERBOSE: [2019-10-04 11:19:14Z] [ERROR] ADMIN0121: An attempt to update service settings failed because the data set that was used for updating was stale. Refresh the data in your session or console view, and then try the update again.
+        # cADFSFarm CreateADFSFarm
         # {
-        #     FederationServiceName        = "$ADFSSiteName.$DomainFQDN"
-        #     FederationServiceDisplayName = "$ADFSSiteName.$DomainFQDN"
-        #     CertificateName              = "$ADFSSiteName.$DomainFQDN"
-        #     SigningCertificateName       = "$ADFSSiteName.Signing"
-        #     DecryptionCertificateName    = "$ADFSSiteName.Decryption"
-        #     ServiceAccountCredential     = $AdfsSvcCredsQualified
-        #     Credential                   = $DomainCredsNetbios
-        #     PsDscRunAsCredential         = $DomainCredsNetbios
-        #     DependsOn                    = "[WindowsFeature]AddADFS"
+        #     ServiceCredential         = $AdfsSvcCredsQualified
+        #     InstallCredential         = $DomainCredsNetbios
+        #     DisplayName               = "$ADFSSiteName.$DomainFQDN"
+        #     ServiceName               = "$ADFSSiteName.$DomainFQDN"
+        #     CertificateName           = "$ADFSSiteName.$DomainFQDN"
+        #     SigningCertificateName    = "$ADFSSiteName.Signing"
+        #     DecryptionCertificateName = "$ADFSSiteName.Decryption"
+        #     Ensure                    = 'Present'
+        #     PsDscRunAsCredential      = $DomainCredsNetbios
+        #     DependsOn                 = "[WindowsFeature]AddADFS"
         # }
+
+        AdfsFarm CreateADFSFarm
+        {
+            FederationServiceName        = "$ADFSSiteName.$DomainFQDN"
+            FederationServiceDisplayName = "$ADFSSiteName.$DomainFQDN"
+            CertificateName              = "$ADFSSiteName.$DomainFQDN"
+            SigningCertificateName       = "$ADFSSiteName.Signing"
+            DecryptionCertificateName    = "$ADFSSiteName.Decryption"
+            ServiceAccountCredential     = $AdfsSvcCredsQualified
+            Credential                   = $DomainCredsNetbios
+            DependsOn                    = "[WindowsFeature]AddADFS"
+        }
 
         ADFSRelyingPartyTrust CreateADFSRelyingParty
         {
