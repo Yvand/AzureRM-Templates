63a64c9d93315763473c432146cf5ddf3ddb9776
Yvan Duhamel <yvandev@outlook.fr>
2022-04-14 15:49:28 +0200

    Update entrypoint.ps1

--- action-armttk/entrypoint.ps1
+++ action-armttk/entrypoint.ps1
@@ -1,5 +1,8 @@
 Import-Module "${Env:ARMTTK_PATH}/arm-ttk/arm-ttk.psd1"
 
+# Remove arm-ttk test that causes an error for DevTest Labs templates, because they cannot use deployment().properties.templateLink.uri - https://github.com/Azure/azure-devtestlab/issues/833
+Remove-Item -Path "${Env:ARMTTK_PATH}/arm-ttk/testcases/deploymentTemplate/artifacts-parameter.test.ps1" -Confirm:$false
+
 $testResults = $null
 #$testResults = Test-AzTemplate -TemplatePath "/github/workspace/Templates/SharePoint-ADFS"
 $directories = Get-ChildItem -Path "/github/workspace/Templates" -Recurse -Filter "azuredeploy.parameters.json" | %{[System.IO.Path]::GetDirectoryName($_)}
