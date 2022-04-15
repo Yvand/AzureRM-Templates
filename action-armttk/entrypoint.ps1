8d736aeea27404f3caf37734f49fce7f7614f20c
Yvan Duhamel <yvandev@outlook.fr>
2022-04-15 11:36:38 +0200

    Update entrypoint.ps1

--- action-armttk/entrypoint.ps1
+++ action-armttk/entrypoint.ps1
@@ -7,18 +7,18 @@ $testResults = $null
 #$testResults = Test-AzTemplate -TemplatePath "/github/workspace/Templates/SharePoint-ADFS"
 $directories = Get-ChildItem -Path "/github/workspace/Templates" -Recurse -Filter "azuredeploy.parameters.json" | %{[System.IO.Path]::GetDirectoryName($_)}
 foreach ($directory in $directories) {
-	$testResults += Test-AzTemplate -TemplatePath $directory
+    $testResults += Test-AzTemplate -TemplatePath $directory
 }
 
 $testErrors =  $testResults | Where-Object {$false -eq $_.Passed}
 if ($null -eq $testErrors) {
-	Write-Host "All tests passed"
+    Write-Host "All tests passed"
     exit 0
 } 
 else {
-	Write-Host "The following files did not pass the Azure Resource Manager Template Toolkit tests:"
+    Write-Host "The following files did not pass the Azure Resource Manager Template Toolkit tests:"
     $testErrors.File.FullPath | Select-Object -Unique
     Write-Host "Errors:"
     Write-Output $testErrors
-    exit 1    
+    exit 1
 }
