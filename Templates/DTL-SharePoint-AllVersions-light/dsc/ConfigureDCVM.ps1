e7646b01fa66cc061a8f9aa1794171a22cb4250e
Yvan Duhamel <yvandev@outlook.fr>
2022-07-05 17:39:35 +0200

    update script EnforceLdapAuthOverTls on DC

--- Templates/DTL-SharePoint-AllVersions-light/dsc/ConfigureDCVM.ps1
+++ Templates/DTL-SharePoint-AllVersions-light/dsc/ConfigureDCVM.ps1
@@ -365,8 +365,12 @@
             SetScript  = {
                 $domain = Get-ADDomain -Current LocalComputer
                 $key = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\NTDS\Parameters"
-                New-GPO -name "LDAP_LdapEnforceChannelBinding" -comment "GPO For LdapEnforceChannelBinding" | Set-GPRegistryValue -key $key -ValueName "LdapEnforceChannelBinding" -Type DWORD -value 2 |New-GPLink -Target $domain.DomainControllersContainer -order 1
-                New-GPO -name "LDAP_LDAPServerIntegrity" -comment "GPO For LDAPServerIntegrity" | Set-GPRegistryValue -key $key -ValueName "ldapserverintegrity" -Type DWORD -value 2 | New-GPLink -Target $domain.DomainControllersContainer -order 1
+                if ($null -eq (Get-GPO -Name "LDAP_LdapEnforceChannelBinding" -ErrorAction SilentlyContinue)) {
+                    New-GPO -name "LDAP_LdapEnforceChannelBinding" -comment "GPO For LdapEnforceChannelBinding" | Set-GPRegistryValue -key $key -ValueName "LdapEnforceChannelBinding" -Type DWORD -value 2 |New-GPLink -Target $domain.DomainControllersContainer -order 1
+                }
+                if ($null -eq (Get-GPO -Name "LDAP_LDAPServerIntegrity" -ErrorAction SilentlyContinue)) {
+                    New-GPO -name "LDAP_LDAPServerIntegrity" -comment "GPO For LDAPServerIntegrity" | Set-GPRegistryValue -key $key -ValueName "ldapserverintegrity" -Type DWORD -value 2 | New-GPLink -Target $domain.DomainControllersContainer -order 1
+                }
             }
             GetScript  = { return @{ "Result" = "false" } }
             TestScript = { return $false }
