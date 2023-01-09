1147db2fdcf7d6264ffe38413f6f219ec0536ae1
Yvan Duhamel <yvandev@outlook.fr>
2023-01-09 13:54:26 +0100

    update dsc

--- Templates/DTL-SharePoint-AllVersions-light/dsc/ConfigureSPLegacy.ps1
+++ Templates/DTL-SharePoint-AllVersions-light/dsc/ConfigureSPLegacy.ps1
@@ -24,7 +24,7 @@ configuration ConfigureSPVM
     Import-DscResource -ModuleName NetworkingDsc -ModuleVersion 9.0.0
     Import-DscResource -ModuleName ActiveDirectoryDsc -ModuleVersion 6.2.0
     Import-DscResource -ModuleName xCredSSP -ModuleVersion 1.4.0
-    Import-DscResource -ModuleName WebAdministrationDsc -ModuleVersion 4.0.0
+    Import-DscResource -ModuleName WebAdministrationDsc -ModuleVersion 4.1.0
     Import-DscResource -ModuleName SharePointDsc -ModuleVersion 5.3.0
     Import-DscResource -ModuleName DnsServerDsc -ModuleVersion 3.0.0
     Import-DscResource -ModuleName CertificateDsc -ModuleVersion 5.1.0
