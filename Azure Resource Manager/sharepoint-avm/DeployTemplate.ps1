b76d909cb951e70392059d6e2d9eadca42a7d681
Yvan Duhamel <yvandev@outlook.fr>
2025-03-27 11:02:03 +0100

    update tasks

--- Azure Resource Manager/sharepoint-avm/DeployTemplate.ps1
+++ Azure Resource Manager/sharepoint-avm/DeployTemplate.ps1
@@ -1,3 +1,5 @@
+#Requires -Modules @{ ModuleName="Az.Resources"; ModuleVersion="7.8.0" } # min version to fix https://github.com/Azure/azure-powershell/issues/26752
+
 param(
     [string] $resourceGroupLocation = "francecentral",
     [string] $resourceGroupName,
@@ -6,6 +8,9 @@ param(
 
 $deploymentName = "sharepoint-{0:yyMMdd-HHmm}" -f (Get-Date)
 $templateParametersFileName = 'main.bicepparam'
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 
 Write-Host "Deploying '$deploymentName' to '$resourceGroupName' in '$resourceGroupLocation'"
-az deployment sub create --name $deploymentName --location $resourceGroupLocation --parameters $templateParametersFileName --parameters resourceGroupName="$resourceGroupName" adminPassword="$password" otherAccountsPassword="$password"
+#az deployment sub create --name $deploymentName --location $resourceGroupLocation --parameters $templateParametersFileName --parameters resourceGroupName="$resourceGroupName" adminPassword="$password" otherAccountsPassword="$password"
+New-AzDeployment -Name $deploymentName -Location $resourceGroupLocation -TemplateParameterFile $templateParametersFileName `
+    -resourceGroupName $resourceGroupName -adminPassword $securePassword -otherAccountsPassword $securePassword
