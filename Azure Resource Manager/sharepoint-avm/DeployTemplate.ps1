9f0902687ed1c2f6501142c25be795fef3d01a7a
Yvan Duhamel <yvandev@outlook.fr>
2025-03-27 17:25:19 +0100

    update tasks

--- Azure Resource Manager/sharepoint-avm/DeployTemplate.ps1
+++ Azure Resource Manager/sharepoint-avm/DeployTemplate.ps1
@@ -10,7 +10,10 @@ $deploymentName = "sharepoint-{0:yyMMdd-HHmm}" -f (Get-Date)
 $templateParametersFileName = 'main.bicepparam'
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 
-Write-Host "Deploying '$deploymentName' to '$resourceGroupName' in '$resourceGroupLocation'"
+Write-Host "Deploying '$deploymentName' to '$resourceGroupName' in '$resourceGroupLocation'" -ForegroundColor Green
+$startTime = $(Get-Date)
 #az deployment sub create --name $deploymentName --location $resourceGroupLocation --parameters $templateParametersFileName --parameters resourceGroupName="$resourceGroupName" adminPassword="$password" otherAccountsPassword="$password"
 New-AzDeployment -Name $deploymentName -Location $resourceGroupLocation -TemplateParameterFile $templateParametersFileName `
     -resourceGroupName $resourceGroupName -adminPassword $securePassword -otherAccountsPassword $securePassword
+$elapsedTime = New-TimeSpan $startTime $(get-date)
+Write-Host "Deployment completed successfully in $($elapsedTime.ToString("h\hmm\m\n"))." -ForegroundColor Green
