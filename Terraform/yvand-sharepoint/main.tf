2016ec2a241254840a602a7b612223021eaa3238
Yvand <yvandev@outlook.fr>
2025-04-24 13:41:38 +0200

    update sample

--- Terraform/yvand-sharepoint/main.tf
+++ Terraform/yvand-sharepoint/main.tf
@@ -1,3 +1,10 @@
+variable "resource_group_name" {}
+variable "subscription_id" {}
+variable "admin_password" {}
+variable "other_accounts_password" {}
+variable "rdp_traffic_rule" {}
+# variable "_artifactsLocation" {}
+
 module "sharepoint" {
   source                                = "Yvand/sharepoint/azurerm"
   location                              = "France Central"
--- Terraform/yvand-sharepoint/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "resource_group_name" {}
-variable "subscription_id" {}
-variable "admin_password" {}
-variable "other_accounts_password" {}
-variable "rdp_traffic_rule" {}
-# variable "_artifactsLocation" {}
