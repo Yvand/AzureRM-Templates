f91150e417e16d9290f7a88cbbec414e154f80c2
Yvand <yvandev@outlook.fr>
2025-10-24 15:11:19 +0200

    update sample

--- Terraform/yvand-sharepoint/main.tf
+++ Terraform/yvand-sharepoint/main.tf
@@ -12,10 +12,10 @@ module "sharepoint" {
   location                              = "francecentral"
   subscription_id                       = var.subscription_id
   resource_group_name                   = var.resource_group_name
-  sharepoint_version                    = "Subscription-RTM" #"Subscription-Latest" #"2019"
+  sharepoint_version                    = "Subscription-Latest" #"2019"
   outbound_access_method                = "PublicIPAddress"
   rdp_traffic_rule                      = var.rdp_traffic_rule
-  enable_azure_bastion                  = false
+  enable_azure_bastion                  = true
   admin_username                        = "yvand"
   admin_password                        = var.admin_password
   other_accounts_password               = var.other_accounts_password
